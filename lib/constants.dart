e5e8720a1bc3361c1b3daed048210380a1caece5
Mo <mo.ghorb@gmail.com>
2025-05-20 06:03:53 +0330

    refactor: update app name from 'Crypto Price Tracker' to 'Crypto Assets Manager'

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appVersion = "v1.5";
-  static const String appName = "Crypto Price Tracker";
+  static const String appName = "Crypto Assets Manager";
   
   // Full app name with version
   static String get appNameWithVersion => "$appName $appVersion";
