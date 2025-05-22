97f7ad36a5c1f2a04c8d6d5459219b29853c8dee
Mo <mo.ghorb@gmail.com>
2025-05-23 00:41:01 +0330

    feat: adds ci

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String appVersion = "v1.6.2";
+  static const String appVersion = "v1.6.3";
   static const String appName = "Crypto Assets Manager";
   
   // Full app name with version
